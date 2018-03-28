3c9532333130872aa9237064a620f64ff49f3d48
szymtur <szymtur@gmail.com>
2018-03-28 16:47:27 +0200

    touchSwipe

--- js/main.js
+++ js/main.js
@@ -215,7 +215,7 @@ function topFunction(){
 /* Function swipe carousel on touch screen*/
 function touchSwipe(){
 
-/*$(".carousel").swipe({
+$(".carousel").swipe({
     swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
 
             if (direction == 'left') {
@@ -225,9 +225,9 @@ function touchSwipe(){
             $(this).carousel('prev');
             }
         }
-    });*/
+    });
 
-$(".carousel").on("touchstart", function(event){
+/*$(".carousel").on("touchstart", function(event){
         var xClick = event.originalEvent.touches[0].pageX;
     $(this).one("touchmove", function(event){
         var xMove = event.originalEvent.touches[0].pageX;
@@ -241,7 +241,7 @@ $(".carousel").on("touchstart", function(event){
     $(".carousel").on("touchend", function(){
             $(this).off("touchmove");
     });
-});
+});*/
 
 
 
