d42277d80ef40eb951c70af1ca6e48f6538b4c6c
szymtur <szymtur@gmail.com>
2018-03-28 19:29:24 +0200

    touchSwipe

--- js/main.js
+++ js/main.js
@@ -218,10 +218,10 @@ function touchSwipe() {
     $(".carousel").on("touchstart", function (event) {
         var xClick = event.originalEvent.touches[0].pageX;
         $(this).one("touchmove", function (event) {
-            var xMove = event.originalEvent.touches[0].pageX;
-            if (Math.floor(xClick - xMove) > 5) {
+            var xMove = event.originalEvent.touches[0].paeX;
+            if (Math.floor(xClick - xMove) > 15) {
                 $(this).carousel('next');
-            } else if (Math.floor(xClick - xMove) < -5) {
+            } else if (Math.floor(xClick - xMove) < -15) {
                 $(this).carousel('prev');
             }
         });
