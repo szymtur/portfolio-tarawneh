af671399a57ac78346c14cce70bdfdcb26d70139
Szymon Tarawneh <szymtur@gmail.com>
2024-11-17 18:08:43 +0100

    gm deprecation fix

--- js/isMobile.js
+++ js/isMobile.js
@@ -1,28 +1,22 @@
-let isMobile = {
-    Android: function() {
-        return (/Android/i).test(navigator.userAgent);
-    },
-    BlackBerry: function() {
-        return (/BlackBerry|RIM|BB|PlayBook/i).test(navigator.userAgent);
-    },
-    iOS: function() {
-        return (/iPhone|iPad|iPod/i).test(navigator.userAgent);
-    },
-    Opera: function() {
-        return (/Opera Mini/i).test(navigator.userAgent);
-    },
-    Windows: function() {
-        return (/IEMobile|WPDesktop|Nokia|Windows Phones/i).test(navigator.userAgent);
-    },
-    webOS: function() {
-        return (/webOS/i).test(navigator.userAgent);
-    },
-    Kindle: function() {
-        return (/Kindle|Silk|KFAPW|KFARWI|KFASWI|KFFOWI|KFJW|KFMEWI|KFOT|KFSAW|KFSOWI|KFTBW|KFTHW|KFTT|WFFOWI/i).test(navigator.userAgent);
-    },
+'use strict';
+
+const screenSizeDetection = window.matchMedia("(max-width: 767px)").matches;
+
+const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+const userAgentDetection = {
+    Android: () => (/Android/i).test(userAgent),
+    BlackBerry: () => (/BlackBerry|RIM|BB|PlayBook/i).test(userAgent),
+    iOS: () => (/iPhone|iPad|iPod/i).test(userAgent),
+    Opera: () => (/Opera Mini/i).test(userAgent),
+    Windows: () => (/IEMobile|WPDesktop|Nokia|Windows Phones/i).test(userAgent),
+    webOS: () => (/webOS/i).test(userAgent),
+    Kindle: () => (/Kindle|Silk|KFAPW|KFARWI|KFASWI|KFFOWI|KFJW|KFMEWI|KFOT|KFSAW|KFSOWI|KFTBW|KFTHW|KFTT|WFFOWI/i).test(userAgent),
+
     any: function() {
-        return (
-            this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows() || this.webOS() || this.Kindle()
-        );
+        return this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows() || this.webOS() || this.Kindle();
     }
 };
+
+
+window.isMobileDevice = () => userAgentDetection.any() || screenSizeDetection;
